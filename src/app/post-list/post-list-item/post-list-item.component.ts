3bd7b1b74ab9b0cca7d697389ffe421ba52ba8c2
Samuel Henz <samuel.henz@di-mob-a.com>
2019-05-15 12:17:06 +0200

    Use PostListItemComponent in PostListComponent

--- src/app/post-list/post-list-item/post-list-item.component.ts
+++ src/app/post-list/post-list-item/post-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from 'src/app/models/post.model';
 
 @Component({
   selector: 'app-post-list-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list-item.component.scss']
 })
 export class PostListItemComponent implements OnInit {
+
+  @Input() post: Post;
 
   constructor() { }
 
